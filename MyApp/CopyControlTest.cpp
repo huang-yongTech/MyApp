56471088221b4bc07d2cb503316515ffd7fde775
huangyong <756655135@qq.com>
2019-03-23 23:11:18 +0800

    拷贝控制

--- MyApp/CopyControlTest.cpp
+++ MyApp/CopyControlTest.cpp
@@ -1,13 +1,59 @@
 #include "pch.h"
 #include "CopyControlTest.h"
+#include <iostream>
 
-CopyControlTest::CopyControlTest(CopyControlTest &) {
-
+CopyControlTest::CopyControlTest() {
+	cout << "调用无参数构造函数" << endl;
 }
 
-CopyControlTest & CopyControlTest::operator=(const CopyControlTest&) {
+CopyControlTest::CopyControlTest(string s, string *ptr) :s(s), ptr(ptr) {
+	cout << "调用带参数的构造函数" << endl;
+}
 
+CopyControlTest::CopyControlTest(const CopyControlTest &copyControl) {
+	cout << "调用拷贝构造函数" << endl;
+
+	//浅拷贝
+	//s = copyControl.s;
+	//ptr = copyControl.ptr;
+	
+	//深拷贝
+	s = copyControl.s;
+	ptr = new string();
+	*ptr = *(copyControl.ptr);
+}
+
+CopyControlTest & CopyControlTest::operator=(const CopyControlTest &copyControl) {
+	cout << "调用拷贝赋值运算符" << endl;
+
+	//浅拷贝
+	//s = copyControl.s;
+	//ptr = copyControl.ptr;
+
+	//深拷贝
+	s = copyControl.s;
+	ptr = new string();
+	*ptr = *(copyControl.ptr);
+
+	return *this;
+}
+
+void CopyControlTest::print() { 
+	cout << "打印第一个成员：" << s << endl;
+	cout << "打印第二个成员：" << *ptr << endl;
 }
 
 CopyControlTest::~CopyControlTest() {
+	if (ptr != NULL) {
+
+		cout << "删除指针" << endl;
+		delete ptr;
+	}
+}
+
+CopyControlTest & copyControl(CopyControlTest copyControl) {
+
+	CopyControlTest controlTest(copyControl);
+
+	return controlTest;
 }
--- MyApp/CopyControlTest.h
+++ MyApp/CopyControlTest.h
@@ -1,19 +1,38 @@
 #pragma once
+#include <string>
 
 using namespace std;
 
 class CopyControlTest {
+
+friend CopyControlTest & copyControl(CopyControlTest copyControl);
+
 public:
 	//默认构造函数
-	CopyControlTest() = default;
+	CopyControlTest();
+
+	//构造函数
+	CopyControlTest(string s, string *ptr);
 
 	//拷贝构造函数
-	CopyControlTest(CopyControlTest &);
+	CopyControlTest(const CopyControlTest &);
 
 	//拷贝赋值运算符
 	CopyControlTest & operator=(const CopyControlTest &);
 
+	//打印
+	void print();
+
 	//析构函数
 	~CopyControlTest();
+
+private:
+	//普通成员变量
+	string s;
+	//指针成员变量
+	string *ptr;
 };
+
+//拷贝
+CopyControlTest & copyControl(CopyControlTest copyControl);
 
